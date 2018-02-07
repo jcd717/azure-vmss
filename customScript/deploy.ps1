a961210e324d511f328e85e1cc8d1e43dba5c2df
jcd717 <30497242+jcd717@users.noreply.github.com>
2018-02-07 16:14:57 +0100

    Update deploy.ps1

--- customScript/deploy.ps1
+++ customScript/deploy.ps1
@@ -1,1 +1,15 @@
 # install IIS & télécharge le contenu
+
+Install-WindowsFeature Web-Server,Web-Asp-Net45
+
+cd c:/inetpub/wwwroot
+rm -Recurse *
+
+mkdir c:/tmp
+cd c:/tmp
+Invoke-WebRequest https://raw.githubusercontent.com/jcd717/azure-vmss/master/AppliWeb.zip -OutFile AppliWeb.zip
+Expand-Archive -LiteralPath ./AppliWeb.zip -DestinationPath .
+cp -Recurse .\AppliWeb\* C:\inetpub\wwwroot
+
+cd /
+rm -Recurse tmp
