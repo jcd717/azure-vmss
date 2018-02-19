17c4e4266aa3939290069a9d569dee68b6d05842
jcd717 <30497242+jcd717@users.noreply.github.com>
2018-02-19 10:45:11 +0100

    Add files via upload

---
+++ install-linux-php/primes.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<?php
+    session_start();
+    if( !isset($_SESSION["nbRefreshs"]) || empty($_SESSION["nbRefreshs"]) ) {
+        $_SESSION["nbRefreshs"]=0;
+        $_SESSION["listPrimes"]=array();
+    }
+
+    // Rempli listPrimes & retourne true si $n est premier
+    function isPrime($n) {
+        if($n==1) return false;
+        foreach($_SESSION["listPrimes"] as $v) {
+            if($n % $v == 0) return false;
+        }
+        $_SESSION["listPrimes"][]=$n;
+        return true;
+    }
+
+    $hostname=gethostname();
+    $ip=gethostbyname($hostname);
+
+    $_SESSION["nbRefreshs"]++;
+
+    $textRefreshs= $_SESSION["nbRefreshs"] . " qui ";
+    $textRefreshs .=isPrime($_SESSION["nbRefreshs"]) ? "EST " : "n'est PAS ";
+    $textRefreshs .="premier.";
+
+    $textListPrimes="";
+    foreach($_SESSION["listPrimes"] as $v) $textListPrimes .=$v . ", ";
+    $textListPrimes=substr($textListPrimes,0,-2);
+?>
+
+<html>
+<head>
+<title>L'hôte et des nombres</title>
+<meta charset="utf-8">
+<style>
+    h1 {
+        text-align: center;       
+    }
+    .textRefreshs {
+        font-size: 1.6em;
+    }
+</style>
+</head>
+
+<body>
+    <h1>
+        Hôte: <?php echo $hostname . " (" . $ip . ")"; ?>
+    </h1>
+    <p><div class="textRefreshs">
+        Nombre de "Refreshs": <?php echo $textRefreshs ?>
+    </div></p>
+    Liste des nombres premiers trouvés (<?php echo count($_SESSION["listPrimes"]); ?>): <?php echo $textListPrimes; ?>
+</body>
+</html>
+
